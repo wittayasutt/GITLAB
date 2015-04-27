2f3b994a9f5a939a1c0c4143b2f2a37c546b24c6
suertle <tletommeng@gmail.com>
2015-04-28 01:44:57 +0700

    update#3

--- Program1.c
+++ Program1.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main()
-{
-    printf("Hello world!\n");
+{   int a;
+    printf("Welcome to my world!\n");
     printf("My name is wittayasutt, I'm User A.\n");
     printf("fixed\n");
     return 0;
