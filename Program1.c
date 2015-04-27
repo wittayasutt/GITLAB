8bd50c6811198877d4600b078223f6eeb8f9f764
wittayasutt <wittayasutt@gmail.com>
2015-04-28 00:38:27 +0700

    A update#1

---
+++ Program1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is wittayasutt, I'm User A.\n");
+    return 0;
+}
---
+++ Program2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is suetle, I'm User B.\n");
+    return 0;
+}
