20812114395460944f279dc9c9724cb70e7576b7
wittayasutt <wittayasutt@gmail.com>
2015-04-28 02:07:30 +0700

    A update#3

--- Program1.c
+++ Program1.c
@@ -5,6 +5,7 @@ int main()
 {
     printf("Hello world!\n");
     printf("My name is wittayasutt, I'm User A.\n");
-    printf("fixed\n");
+    printf("fixed A\n");
+    printf("fixed 2\n");
     return 0;
 }
